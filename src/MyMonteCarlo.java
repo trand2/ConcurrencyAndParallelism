32a334a53c581b93b35ce5134e0a139ec9a13cc7
TRand2 <tylerrandolph123@gmail.com>
2018-10-26 13:53:46 -0600

    added parallel part

--- src/MyMonteCarlo.java
+++ src/MyMonteCarlo.java
@@ -1,4 +1,6 @@
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyMonteCarlo {
     public static void main(String[] args) {
@@ -7,7 +9,7 @@ public class MyMonteCarlo {
     }
 
     public static void sequentialMonteCarlo() {
-        int nThrows = 100000;
+        int nThrows = 1000;
         long startTime = System.currentTimeMillis();
         double x = 0, y = 0;
         int nSuccess = 0;
@@ -19,10 +21,49 @@ public class MyMonteCarlo {
         }
         double value = 4.0 * nSuccess / nThrows;
         long stopTime = System.currentTimeMillis();
-        System.out.println("Approx value:" + value);
-        System.out.println("Difference to exact value of pi: " + (value - Math.PI));
-        System.out.println("Error: " + (value - Math.PI) / Math.PI * 100 + " %");
-        System.out.println("Time Duration: " + (stopTime - startTime) + "ms");
+        System.out.println("Approx value: " + value);
+        System.out.println("Time Duration: " + (stopTime - startTime) + "ms\n");
+
+
+        PiMonteCarlo PiVal = new PiMonteCarlo(nThrows);
+        long startTime2 = System.currentTimeMillis();
+        double value2 = PiVal.getPi();
+        long stopTime2 = System.currentTimeMillis();
+        System.out.println("Approx value: " + value);
+        System.out.println("Time Duration: " + (stopTime2 - startTime2) + "ms");
+    }
+}
+
+class PiMonteCarlo {
+    AtomicInteger nAtomSuccess;
+    int nThrows;
+    double value;
+    class MonteCarlo implements Runnable {
+        @Override
+        public void run() {
+            double x = Math.random();
+            double y = Math.random();
+            if (x * x + y * y <= 1)
+                nAtomSuccess.incrementAndGet();
+        }
+    }
+    public PiMonteCarlo(int i) {
+        this.nAtomSuccess = new AtomicInteger(0);
+        this.nThrows = i;
+        this.value = 0;
+    }
+    public double getPi() {
+        int nProcessors = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newWorkStealingPool(nProcessors);
+        for (int i = 1; i <= nThrows; i++) {
+            Runnable worker = new MonteCarlo();
+            executor.execute(worker);
+        }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+        value = 4.0 * nAtomSuccess.get() / nThrows;
+        return value;
     }
 }
 
